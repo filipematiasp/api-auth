32213e323fa0c878a70aabd5f841f70cdb315bfa
Matias <filipe.matias@globalbot.com.br>
2022-05-05 21:56:33 -0300

    Add Jest Test

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 import UserController from '../controllers/userController.js'
 
-router.post('/register', UserController.register)
+router.post('/register',UserController.register)
 router.post('/login', UserController.login)
 
 export default router
---
+++ test/server.test.js
@@ -0,0 +1,7 @@
+const supertest = require('supertest')
+
+const request = supertest('http://localhost:3000')
+
+test('Deve responder na porta 3000', () => {
+  return request.get('/user').then(res => expect(res.status).toBe(200))
+})
